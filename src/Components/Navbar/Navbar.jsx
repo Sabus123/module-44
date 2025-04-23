ed4a4c5182d2b10000e14eecf2d44417c19f3433
unknown <msm01777946259@gmail.com>
2025-04-24 01:56:14 +0600

    this is secound commint

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
+
 
 
 const navigationData = [
@@ -32,11 +34,31 @@ const navigationData = [
 
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+    
+    const Links = navigationData.map(route => <Link key={route.id} route={route}></Link>);
+
     return (
-        <nav>
-            <ul className='flex'>
+        <nav className='flex justify-between mx-10'>
+
+            <span className='flex' onClick={() => setOpen(!open)}>
                 {
-                    navigationData.map(route => <Link key={route.id} route={route}></Link>)
+                    open ?
+                        <X className='md:hidden'></X> :
+                        <Menu className='md:hidden'></Menu>
+                }
+              <ul className='md:hidden'>
+                {
+                    Links
+                }
+              </ul>
+                <h3 className='ml-4'>My Navbar</h3>
+            </span>
+
+            <ul className='md:flex hidden'>
+                {
+                   Links 
                 }
             </ul>
 
@@ -53,6 +75,8 @@ const Navbar = () => {
                 <li className='ml-10'><a href="/about">About</a></li>
                 <li className='ml-10'><a href="/blog">Blog</a></li>
             </ul> */}
+
+            <button className='bg-blue-400 text-xl border-amber-700 rounded p-2 border-4'>Sign In</button>
         </nav>
     );
 };
