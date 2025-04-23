65e39b3cc06df51bb5320c770cc7e790e164229d
unknown <msm01777946259@gmail.com>
2025-04-23 15:47:14 +0600

    this is the frist commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import DaisyNab from './Components/DaisyNab/DaisyNab'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <> 
+      <header>
+        <Navbar></Navbar>
+      <DaisyNab></DaisyNab>
+      </header>
+      <main>
+
+      </main>
+     
+    </>
+  )
+}
+
+export default App
